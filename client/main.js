8c0c94803b6a17cff37346aba0518d418ccc22e9
nafidurmus <nafidurmus07@gmail.com>
2018-08-25 13:20:47 +0300

    build dapps app with web3 and meteor

---
+++ client/main.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    web3.eth.getBalance("0x96CD7da5b546F5B23542466d58F27ddC685E442F", 
+    	function(err,res){
+    		alert(res);
+    	})
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
